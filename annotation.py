7153c9e5518cfba954a2449f7f949863769eed4e
OpenTokix <petereriksson.net@gmail.com>
2020-02-29 14:51:27 +0100

    Working. not sure about positional arguments

--- annotation.py
+++ annotation.py
@@ -9,9 +9,10 @@ parser = argparse.ArgumentParser(prog='Annotation poster', description="Get some
 parser.add_argument('--port', '-p', type=int, help="port of elasticsearch server")
 parser.add_argument('--host', '-H', type=str, help="Host of elasticsearch server")
 parser.add_argument('--index', '-i', type=str, help="Index for annotations")
-parser.add_argument('message')
-parser.add_argument('tags')
+parser.add_argument('--verbose', nargs='?')
 parser.add_argument('invoker')
+parser.add_argument('tags')
+parser.add_argument('message')
 options = parser.parse_args()
 
 def main():
@@ -23,8 +24,14 @@ def main():
     'tags': tags,
     'timestamp': datetime.utcnow()
   }
-  result = es.index(index=options.index, body=doc)
-  print(result['result'])
+  try:
+    result = es.index(index=options.index, body=doc)
+  except Error as e:
+    print("Error: ", e)
+    sys.exit(1)
+  
+  if options.verbose:
+    print(result['result'])
 
 if __name__ == "__main__":
   main()
